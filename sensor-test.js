b3a82bced92a30a8e835728de79164dece8aa6ca
Alexandre Ferreira <alexandresgf@gmail.com>
2016-04-08 15:07:43 -0300

    Sensor test file

---
+++ sensor-test.js
@@ -0,0 +1,20 @@
+var sensorLib = require('node-dht-sensor');
+
+var sensor = {
+    initialize: function () {
+        return sensorLib.initialize(11, 24);
+    },
+    read: function () {
+        var readout = sensorLib.read();
+        console.log('Temperature: ' + readout.temperature.toFixed(2) + 'C, ' + 'humidity: ' + readout.humidity.toFixed(2) + '%');
+        setTimeout(function () {
+            sensor.read();
+        }, 10000);
+    }
+};
+
+if (sensor.initialize()) {
+    sensor.read();
+} else {
+    console.warn('Failed to initialize sensor');
+}
